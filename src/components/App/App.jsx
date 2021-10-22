885b0aa0cceeff29af6c5de5e16826cbac4e23c1
Nick Allen <rutherford.t.b@gmail.com>
2021-10-22 16:22:35 -0500

    receive correct GET response

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import Axios from 'axios';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [galleryItems, setGalleryItems] = useState([]);
+
+
+  const getPhotos = () => {
+    Axios.get('/gallery')
+      .then(response => {
+        console.log(response);
+        setGalleryItems(response.data);
+      }).catch(response => {
+        console.log('GET error');
+      })
+  }
+
+  useEffect(() => {
+    getPhotos();
+  }, []);
+  
     return (
       <div className="App">
         <header className="App-header">
