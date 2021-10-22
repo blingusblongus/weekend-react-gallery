a833f838a9a6a9f486a97f2af3df276cc056216e
Nick Allen <rutherford.t.b@gmail.com>
2021-10-22 16:10:21 -0500

    updated database with image info

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,9 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/brushTeethCat.png', description: 'Photo of a cat that doesn\'t brush it\'s teeth.', likes: 0 },
+    { id: 3, path: 'images/confusedCat.jpg', description: 'Photo of a cat that has imposter syndrome.', likes: 0 },
+    { id: 4, path: 'images/neckCat.png', description: 'Photo of a cat that\'s a little taken aback.', likes: 0 },
+    { id: 4, path: 'images/sleepyCat.png', description: 'Photo of a very tired cat.', likes: 0 }
 ];
 
 module.exports = galleryItems;
