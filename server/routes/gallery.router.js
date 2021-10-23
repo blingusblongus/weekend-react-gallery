2e55f380f087175f687f15288d0fa8900555e676
Nick Allen <rutherford.t.b@gmail.com>
2021-10-23 16:40:42 -0500

    db PUT working

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -6,22 +6,43 @@ const pool = require('../modules/pool.js');
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
 // PUT Route
+// SERVER DATA
+// router.put('/like/:id', (req, res) => {
+//     console.log(req.params);
+//     const galleryId = req.params.id;
+//     for(const galleryItem of galleryItems) {
+//         if(galleryItem.id == galleryId) {
+//             galleryItem.likes += 1;
+//         }
+//     }
+//     res.sendStatus(200);
+// }); // END PUT Route
+
+// DB PUT
 router.put('/like/:id', (req, res) => {
     console.log(req.params);
     const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
+    const queryText = `UPDATE gallery
+        SET likes = likes + 1
+        WHERE id = $1`;
+    const values = [galleryId];
+
+    pool.query(queryText, values)
+        .then(response => {
+            console.log('likes updated at id:', galleryId);
+            res.sendStatus(200);
+        }).catch(err => {
+            console.log('PUT ERR', err);
+            res.sendStatus(500);
+        });
 }); // END PUT Route
 
-// // GET Route
+// // GET Route SERVER 
 // router.get('/', (req, res) => {
 //     res.send(galleryItems);
 // }); // END GET Route
 
+//GET Route DB
 router.get('/', (req, res) => {
     const queryText = 'SELECT * FROM gallery';
 
