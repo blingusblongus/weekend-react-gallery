bc6bd77dff3b38e5e8f4627d6d0e8c7fba65f925
Nick Allen <rutherford.t.b@gmail.com>
2021-10-23 13:43:56 -0500

    refactored to make it more concise and added comments to GalleryItem

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -3,38 +3,29 @@ import {useState} from 'react';
 
 export default function GalleryItem({item}){
     const [showDescription, setShowDescription] = useState(false);
-    console.log(item.description);
 
+    // flips showDescription bool on image click
     const toggleDescription = () => {
         setShowDescription(!showDescription);
         console.log('description:', showDescription);
     }
 
-    let imageJsx = (
+    // conditionally renders dim and description based on showDescription
+    let jsx = (
         <div className="item-container">
             <img 
-            className="item item-image"
-            src={item.path} 
-            onClick={toggleDescription}
-            alt={item.description}
-            />
-            
-        </div>);
-
-    let descriptionJsx = (
-        <div className="item-container">
-            <img 
-            className="item item-image dim"
+            className={showDescription ?
+                "item item-image dim" :
+                "item item-image"}
             src={item.path} 
             onClick={toggleDescription}
             />
+            {showDescription && 
             <div className="item-description">
                 {item.description}
-            </div>
-            
+            </div>}
         </div>
     )
 
-    // return image or description 
-    return showDescription ? descriptionJsx : imageJsx;
+    return jsx;
 }
