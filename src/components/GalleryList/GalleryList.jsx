85932d86219be371c85177c10d3ac389c6e7a190
Nick Allen <rutherford.t.b@gmail.com>
2021-10-23 13:37:03 -0500

    added flex-flow to GalleryList

--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,10 +1,11 @@
 import GalleryItem from "../GalleryItem/GalleryItem";
+import './GalleryList.css';
 
 export default function GalleryList ({galleryItems, getPhotos}) {
     console.log(galleryItems);
 
     return (
-        <div>
+        <div className="list-container">
             {galleryItems.map(item => {
                 return <GalleryItem key={item.id} item={item}/>
             })}
